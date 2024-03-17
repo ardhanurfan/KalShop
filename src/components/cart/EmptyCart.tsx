3d7634afeb4c8114a2b80f131b9a04a81e60d1f8
Ardhan Nur Urfan <88726616+ardhanurfan@users.noreply.github.com>
2024-03-17 23:38:41 +0700

    style: empty cart, empty history

--- src/components/cart/EmptyCart.tsx
+++ src/components/cart/EmptyCart.tsx
@@ -3,16 +3,23 @@ import { Link } from "react-router-dom";
 
 const EmptyCart = () => {
   return (
-      <Box>
-        <Typography variant="h3" sx={{ m: 4 }}>
-          {`Your cart is empty! :(`}
-        </Typography>
-        <Link to="/">
-          <Button variant="contained" sx={{ m: 4 }}>
-            Add Item to Cart
-          </Button>
-        </Link>
-      </Box>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Typography variant="h3" sx={{ m: 4 }}>
+        {`Your cart is empty! :(`}
+      </Typography>
+      <Link to="/">
+        <Button variant="contained" sx={{ m: 4 }}>
+          Add Item to Cart
+        </Button>
+      </Link>
+    </Box>
   );
 };
 
